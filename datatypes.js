9e2d1a9e16d31f6ec8c51801083a60fab9aefd1e
sabateur007 <40513717+sabateur007@users.noreply.github.com>
2024-06-18 10:02:58 +0000

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,10 @@
+"use strict"  // treats all code in newer verion of js, ab jaroorat nhi hoti waise but knowledge ke liye
+//alert(3+3) we are using nodejs, not browser
+let age = 18
+let name = "aniket"
+let ismale = true
+let jobtype
+let ienvioej = null
+
+console.log(typeof(ienvioej));  // object. hence null is an object type. it is a standalone value.
+console.log(typeof(jobtype))  // ubdefined. this means the variable is declared but not defined.
