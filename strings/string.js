129cf808158d93f496b303b81240e35d867b344d
sabateur007 <40513717+sabateur007@users.noreply.github.com>
2024-06-19 14:50:49 +0000

    strings

---
+++ strings/string.js
@@ -0,0 +1,16 @@
+//use backticks to write complex string sentences in console. thid is known as string interpolation
+let naam = "aniket"
+console.log(`my name is ${naam} and i am a coder`)
+//another way of declaring a string is using object
+let str1 =  new String("singh")
+let str2 = "badmagic"
+// console.log(str1)
+// console.log(typeof str1)
+// console.log(`string name str1 is ${str1}`)
+// console.log(str1[2])
+// console.log(`prototype: ${str1.__proto__}`)
+
+str2[1] = `q`
+console.log(str2)
+str2 = "javascriptstringmagic"
+console.log(str2)
