49ea35fbc457024cf7e1c35c979b77ffd7cdf330
sabateur007 <40513717+sabateur007@users.noreply.github.com>
2024-06-19 08:15:11 +0000

    basic commit 1.

---
+++ basics/operate.js
@@ -0,0 +1,19 @@
+// let str1 = "aniket"
+// let str2 = " singh"
+// let str3 = str1+str2
+// console.log(str3)
+
+// console.log(1+2+"2")
+// console.log("1"+2+2)
+// console.log(1+"2"+2)
+let x=3
+let y=++x
+console.log(`x:${x},y:${y}`)
+let z=x++
+console.log(`x:${x},z:${z}`)
+let a=y++
+console.log(`a:${a},y:${y}`)
+let b=++z
+console.log(`x:${x}, y:${y}, z:${z},a:${a},b"${b}`)
+
+
